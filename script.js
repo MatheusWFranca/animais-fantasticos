efffe89a090dbd3cd93fe59d3e3b159bfbe385d0
Matheus França <mateusanches2@gmail.com>
2021-05-27 00:14:33 -0300

    Scroll Suave

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ function initAccordion() {
   const activeClass = 'ativo';
 
   if (accordionList.length) {
-    accordionList[0].classList.add(activeClass)
+    accordionList[0].classList.add(activeClass);
     accordionList[0].nextElementSibling.classList.add(activeClass);
 
     function activeAccordion() {
@@ -39,5 +39,24 @@ function initAccordion() {
     });
   }
 }
-
 initAccordion();
+
+function initScrollSuave() {
+  const linkInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  }
+
+  linkInternos.forEach((link) => {
+    link.addEventListener('click', scrollToSection);
+  });
+}
+initScrollSuave();
