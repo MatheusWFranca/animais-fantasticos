e63078267c602780b3f4e1600758fc76c173fee6
Matheus França <mateusanches2@gmail.com>
2021-06-30 20:47:47 -0300

    Eslint

--- js/modules/fetch-bitcoin.js
+++ js/modules/fetch-bitcoin.js
@@ -1,10 +1,12 @@
 export default function initFetchBitcoin() {
   fetch('https://blockchain.info/pt/ticker')
-  .then((response) => {
-    return response.json()
-  })
-  .then((bitcoin) => {
-    const btcPreco = document.querySelector('.btc-preco');
-    btcPreco.innerText = (100 / bitcoin.BRL.sell).toFixed(4);
-  });
+    .then((response) => {
+      response.json();
+    })
+    .then((bitcoin) => {
+      const btcPreco = document.querySelector('.btc-preco');
+      btcPreco.innerText = (100 / bitcoin.BRL.sell).toFixed(4);
+    }).catch((error) => {
+      console.log(error);
+    });
 }
--- js/modules/modal.js
+++ js/modules/modal.js
@@ -3,23 +3,23 @@ export default function initModal() {
   const botaoFechar = document.querySelector('[data-modal="fechar"]');
   const containerModal = document.querySelector('[data-modal="container"]');
 
+  function abrirModal(event) {
+    event.preventDefault();
+    containerModal.classList.add('ativo');
+  }
+
+  function fecharModal(event) {
+    event.preventDefault();
+    containerModal.classList.remove('ativo');
+  }
+
+  function clickForaModal(event) {
+    if (event.target === this) {
+      fecharModal(event);
+    }
+  }
+
   if (botaoAbrir && botaoFechar && containerModal) {
-    function abrirModal(event) {
-      event.preventDefault();
-      containerModal.classList.add('ativo');
-    }
-
-    function fecharModal(event) {
-      event.preventDefault();
-      containerModal.classList.remove('ativo');
-    }
-
-    function clickForaModal(event) {
-      if (event.target === this) {
-        fecharModal(event);
-      }
-    }
-
     botaoAbrir.addEventListener('click', abrirModal);
     botaoFechar.addEventListener('click', fecharModal);
     containerModal.addEventListener('click', clickForaModal);
